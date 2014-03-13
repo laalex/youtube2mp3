611c45578cd29b20a13f846796e3183b2b8685a4
Alexandru Andrei <roshkattu94@gmail.com>
2014-03-13 10:57:20 +0200

    Updated gitgnore

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://stardust.alexandrulamba.com/youtube2mp3';
+$config['base_url']	= 'http://zonglist.local';
 
 /*
 |--------------------------------------------------------------------------
