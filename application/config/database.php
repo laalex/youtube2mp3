97da3d99c85a94503190103f56b1470c32252d04
Roshu <roshkattu94@gmail.com>
2014-03-31 18:43:26 +0300

    CHMOD 777 on unix system

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'mp3droid';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'zonglist';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
