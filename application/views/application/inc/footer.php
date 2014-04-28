b9a32052fce67e281f81b7be94a8198e78ee6ad2
Roshu <roshkattu94@gmail.com>
2014-04-28 14:17:29 +0300

    Added google analytics

--- application/views/application/inc/footer.php
+++ application/views/application/inc/footer.php
@@ -83,5 +83,16 @@
 	<script type="text/javascript" src="<?php assets_js();?>youtube_api.js"></script>
 	<script src="https://apis.google.com/js/client.js?onload=loadYTApi"></script>
 	<iframe id="download_frame" style="display:none;width:1px;height:1px;"></iframe>
+	<!-- Analytics -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-48895589-1', 'zonglist.com');
+	  ga('send', 'pageview');
+
+	</script>
 </body>
 </html>
